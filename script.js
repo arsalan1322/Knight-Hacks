bedb372ef6e9ba8b51ad2c34be154e8413d74c78
pillscapsules <pirooz321@gmail.com>
2021-11-14 05:11:07 -0500

    added campfire and ocean themes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.getElementById('container')
 const text = document.getElementById('text')
+//for randomizing video/audio
+var source = document.createElement('source')
+var source2 = document.createElement('source')
 
 const totalTime = 16000
 const breatheTime = totalTime / 4
@@ -37,7 +40,6 @@ const play = document.getElementById("playpause");
 // SELECT AUDIO & VIDEO ELEMENT -yiling
 const audio = document.querySelector(".audio audio");
 const video = document.querySelector(".video video");
-console.log(play.checked)
 function playVideoAudio(){
     if (play.checked != false){
         audio.play();
@@ -54,3 +56,15 @@ function playVideoAudio(){
 play.addEventListener("click", () => {
     playVideoAudio();
   });
+
+const theme = ['stones', 'ocean', 'campfire']
+const randomElement = theme[Math.floor(Math.random() * theme.length)];
+
+//list of video/audio pairs
+source.setAttribute('src', "./assets/video/" + randomElement + '.mp4')
+source.setAttribute('type', 'video/mp4')
+video.appendChild(source)
+
+source2.setAttribute('src', "./assets/audio/" + randomElement + '.mp3')
+source2.setAttribute('type', 'audio/mp3')
+audio.appendChild(source2)
