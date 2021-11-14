5e6954e389fa7cf1a2ce68dcfd265c6137e5ac27
pillscapsules <pirooz321@gmail.com>
2021-11-14 00:14:26 -0500

    added play button

--- script.js
+++ script.js
@@ -31,26 +31,26 @@ setInterval(breatheAnimation, totalTime)
 //start of riyan's part
 //end of riyan's part
 
-// // SELECT PLAY AND PAUSE BUTTON - yiling
-// const play = document.querySelector(".play"),
-//   pause = document.querySelector(".pause");
+// SELECT PLAY ID -pirooz
+const play = document.getElementById("playpause");
 
-// // SELECT AUDIO ELEMENT -yiling
-// const audio = document.querySelector(".audio audio");
-
-
-// // PLAY AUDIO-yiling
-// play.addEventListener("click", () => {
-//     isPlaying = true;
-//     audio.play();
-//   });
-//   // PAUSE AUDIO-yiling
-//   pause.addEventListener("click", () => {
-//     isPlaying = false;
-//     audio.pause();
-//   });
-
+// SELECT AUDIO & VIDEO ELEMENT -yiling
+const audio = document.querySelector(".audio audio");
+const video = document.querySelector(".video video");
+console.log(play.checked)
+function playVideoAudio(){
+    if (play.checked != false){
+        audio.play();
+        video.play();
+    }
+    else{
+        audio.pause();
+        video.pause();
+    }
+}
 
 
-
-
+// PLAY/PAUSE AUDIO -yiling/pirooz
+play.addEventListener("click", () => {
+    playVideoAudio();
+  });
