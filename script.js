78dbbd9c18c1df209815616c8e77b4d27f493b5b
Arsalan Khan <92406122+arsalan1322@users.noreply.github.com>
2021-11-13 19:58:31 -0500

    ITS WORKING

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const container = document.querySelector('.main-container')
-const text = document.querySelector('#tutorial')
+const container = document.getElementById('container')
+const text = document.getElementById('text')
 
 const totalTime = 16000
 const breatheTime = totalTime / 4
@@ -8,22 +8,49 @@ const holdTime = totalTime / 4
 breatheAnimation()
 
 function breatheAnimation(){
-    text.innerHTML = 'BREATHE! BREATHE! (in)'
+    text.innerHTML = 'Breathe In'
     container.className = 'container grow'
 
     setTimeout(() => {
-        text.innerText = 'hold...'
+        text.innerText = 'Hold...'
 
         setTimeout(() => {
-            text.innerText = 'BREATHE! BREATHE! (out)'
+            text.innerText = 'Breathe Out'
             container.className = 'container shrink'
             
             setTimeout(() => {
-                text.innerText = 'hold...'
+                text.innerText = 'Hold...'
 
-            }, breatheTime)
-        }, holdTime)
-    }, breatheTime)
+            }, breatheTime);
+        }, holdTime);
+    }, breatheTime);
 }
 
 setInterval(breatheAnimation, totalTime)
+
+//start of riyan's part
+//end of riyan's part
+
+// SELECT PLAY AND PAUSE BUTTON - yiling
+const play = document.querySelector(".play"),
+  pause = document.querySelector(".pause");
+
+// SELECT AUDIO ELEMENT -yiling
+const audio = document.querySelector(".audio audio");
+
+
+// PLAY AUDIO-yiling
+play.addEventListener("click", () => {
+    isPlaying = true;
+    audio.play();
+  });
+  // PAUSE AUDIO-yiling
+  pause.addEventListener("click", () => {
+    isPlaying = false;
+    audio.pause();
+  });
+
+
+
+
+
